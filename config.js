9bd8345d1a8ab3509fb3fcdd76e0bd6ec2ecde86
Ntanis <me@ntanis.dev>
2022-02-07 11:59:42 +0200

    consistency

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // Critical Scripts | https://criticalscripts.shop
 
 module.exports = {
-    // The public IP address of the proxy server the clients will use to connect to, leaving this to null will work in most cases otherwise set your proxy server's public IP address.
-    'ipAddress': null,
-
     // The port the proxy server will listen to, this port needs to be allowed in the proxy server's firewall on both TCP and UDP protocols.
-    'port': 34540,
+    port: 34540,
 
     // The IP address the proxy server will listen to, leave this to null to automatically listen on all network interfaces.
-    'listeningIpAddress': null,
+    listeningIpAddress: null,
+
+    // The public IP address of the proxy server the clients will use to connect to, leaving this to null will work in most cases otherwise set your proxy server's public IP address.
+    ipAddress: null,
 
     // This is the authentication key, the exact same needs to be set in the resource's config so they match up.
-    'authKey': null,
+    authKey: null,
 
     // If set to true, will add a 'criticalscripts:criticalscripts' credentials pair to the TURN server that it can be used to check the connectivity of it.
-    'debugTurnPair': false
+    debugTurnPair: false
 }
